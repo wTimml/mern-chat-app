2df91db4aa4041358881da452c5dec0ff4945e83
wTimml <wtl25@hotmail.com>
2025-05-12 17:27:51 -0300

    First commit

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -1,7 +1,9 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
 const Auth = () => {
   return (
     <div className="h-[100vh] w-[100vw] flex items-center justify-center">
-      <div className="h-[80vh] bg-surface border-2 border-surface text-opacity-90 shadow-2xl shadow-shadow w-[80vw] md:w-[90vw] lg:w-[70vw] xl:w-[60vw] rounded-3xl grid xl:grid-cols-2">
+      <div className="h-[80vh] bg-surface border-2 border-surface text-opacity-90 shadow-lg shadow-shadow w-[80vw] md:w-[90vw] lg:w-[70vw] xl:w-[60vw] rounded-3xl grid xl:grid-cols-2">
         <div className="flex flex-col grap-10 items-center justify-center">
           <div className="flex items-center justify-center flex-col">
             <div className="flex items-center justify-center">
@@ -12,28 +14,25 @@ const Auth = () => {
               Get started with the chat app!
             </p>
           </div>
-          <div className="flex items-center justify-center w-full">
-            <div className="text-sm font-medium text-center text-gray-500 border-b border-gray-200 dark:text-gray-400 dark:border-gray-700">
-              <ul className="flex flex-wrap -mb-px">
-                <li className="me-2">
-                  <a
-                    href="#"
-                    className="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"
-                  >
-                    Login
-                  </a>
-                </li>
-                <li className="me-2">
-                  <a
-                    href="#"
-                    className="inline-block p-4 text-blue-600 border-b-2 border-blue-600 rounded-t-lg active dark:text-blue-500 dark:border-blue-500"
-                    aria-current="page"
-                  >
-                    Signup
-                  </a>
-                </li>
-              </ul>
-            </div>
+          <div className="flex items-center justify-center w-full pt-4">
+            <Tabs className="w-3/4">
+              <TabsList className="bg-transparent rounded-none w-full">
+                <TabsTrigger
+                  value="login"
+                  className="data-[state=active]:bg-transparent border-b-2 rounded-none w-full data-[state=active]:text-text-primary data-[state=active]:border-b-primary p-3 transition-all duration-300"
+                >
+                  Login
+                </TabsTrigger>
+                <TabsTrigger
+                  value="signup"
+                  className="data-[state=active]:bg-transparent border-b-2 rounded-none w-full data-[state=active]:text-text-primary data-[state=active]:border-b-primary p-3 transition-all duration-300"
+                >
+                  Signup
+                </TabsTrigger>
+              </TabsList>
+              <TabsContent value="login">TEST Login</TabsContent>
+              <TabsContent value="signup">Test Signup</TabsContent>
+            </Tabs>
           </div>
         </div>
       </div>
